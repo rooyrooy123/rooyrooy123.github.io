75ccabf1cc0c2a812c8b5a6f22b67d6e2f88e6dd
rooyrooy123 <rooyrooytest@gmail.com>
2022-03-13 08:45:34 +0900

    made changes

--- pagesetting.js
+++ pagesetting.js
@@ -1,14 +1,12 @@
 if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
-    ret = confirm("スマホ機器を検知したため、専用ページへ移動します。よろしいですか？");
-    if (ret == true){
+
     location.href = '/sp/index.html';
     
     }
 
     if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-        ret = confirm("スマホ機器を検知したため、専用ページへ移動します。よろしいですか？");
-        if (ret == true){
+
           location.href = '/sp/index.html';
-        }
+
     }
 
